b31c856d3b257bfbc68bbb3868a7507d9a1b1b47
afsaints0987 <saints0987@gmail.com>
2023-09-26 00:02:55 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// TLDraw Extension
+
+// Global variables 
+
+let created: boolean = false;
+let tab: ext.tabs.Tab | null = null;
+let window: ext.windows.Window  | null = null;
+let webview: ext.webviews.Webview | null = null;
+
+console.log('This is working')
