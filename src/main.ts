aef9cc6fd3ac4eedc56ffc72115061d969d2a1ca
afsaints0987 <saints0987@gmail.com>
2023-10-01 01:05:42 +0800

    update necessary dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,106 @@
-// TLDraw Extension
+console.log("This is TLDraw Extension updated version");
 
-// Global variables 
+ext.runtime.onEnable.addListener(() => {
+  console.log('Extension Enabled')
+})
 
-let created: boolean = false;
+let created = false;
+let windowFrame: ext.windows.Window | null = null;
 let tab: ext.tabs.Tab | null = null;
-let window: ext.windows.Window  | null = null;
-let webview: ext.webviews.Webview | null = null;
+let webview = null;
+let websession = null;
 
-console.log('This is working')
+ext.runtime.onExtensionClick.addListener(async () => {
+  console.log('Extension Clicked')
+
+  try {
+    // Create tab
+    if (tab) return;
+    tab = await ext.tabs.create({
+      text: "TLDraw",
+      icon: "assets/icon-1024.png",
+      icon_dark: "assets/icon-128-dark.png",
+      closable: true,
+    });
+
+    // Create a window
+    windowFrame = await ext.windows.create({
+      title: "TLDraw",
+      icon: "assets/icon-1024.png",
+      fullscreenable: true,
+      frame: true
+    });
+
+    // Create a Websession
+    // websession = await ext.websessions.create({
+    //   partition: "TLDraw Extension",
+    //   persistent: true,
+    //   global: false,
+    //   cache: true,
+    // });
+
+    const windowSize = await ext.windows.getContentSize(windowFrame.id);
+    webview = await ext.webviews.create({
+      window: windowFrame,
+      // websession,
+      bounds: {
+        x: 0,
+        y: 0,
+        width: windowSize.width,
+        height: windowSize.height,
+      },
+      autoResize: { width: true, height: true },
+    });
+    await ext.webviews.loadFile(webview.id, "index.html");
+    await ext.webviews.attach(webview.id, windowFrame.id)
+
+    created = true;
+  } catch(err){
+    console.error('ext.runtime.onExtensionClick', JSON.stringify(err))
+  }
+  console.log(tab?.id)
+});
+
+// When Tab is clicked
+ext.tabs.onClicked.addListener(async (event) => {
+  console.log(tab)
+  try {
+    if(event.id === tab?.id && windowFrame !== null){
+      await ext.windows.restore(windowFrame.id)
+      await ext.windows.focus(windowFrame.id)
+    }
+  } catch (error){
+    console.log('ext.tabs.onClicked', error)
+  }
+});
+
+// When Tab is close
+ext.tabs.onClickedClose.addListener(async (event) => {
+  console.log(event.id)
+  try {
+    if(event.id === tab?.id){
+      await ext.tabs.remove(tab.id)
+    }
+    if(event.id === windowFrame?.id){
+      await ext.tabs.remove(windowFrame.id)
+    }
+  } catch(error){
+    console.log('ext.tabs.onClickedClose', error)
+  }
+});
+
+// When Window is close
+ext.windows.onClosed.addListener(async (event) => {
+  console.log(event.id)
+  try {
+    if(!windowFrame) return
+  
+    if(event.id === windowFrame.id){
+      await ext.windows.remove(windowFrame.id)
+      windowFrame = null
+    }
+  } catch(error) {
+    console.log('ext.windows.onClosed', error)
+  }
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require("node:path");
 
 module.exports = {
   mode: process.env["NODE_ENV"],
-  entry: "./src/main.ts",
+  entry: {
+    main: "./src/main.ts",
+    bundle: "./react/index.tsx",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     clean: true,
@@ -13,9 +18,14 @@ module.exports = {
       patterns: [
         { from: "src/assets", to: "assets" },
         { from: "src/manifest.json", to: "manifest.json" },
-        { from: "src/index.html", to: "index.html" },
       ],
     }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // Specify your HTML template
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
   ],
   module: {
     rules: [
@@ -23,6 +33,20 @@ module.exports = {
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
         exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        include: path.resolve(__dirname, "react"), // Specify the directory containing your React code
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
       },
     ],
   },
