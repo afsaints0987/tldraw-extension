94b37682aff91ba4f5716d57ecf3a4cc1850a3ee
afsaints0987 <saints0987@gmail.com>
2023-10-02 02:01:03 +0800

    add react app and update manifest.json

--- react/components/Editor.tsx
+++ react/components/Editor.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Tldraw } from '@tldraw/tldraw'
-import "@tldraw/tldraw/tldraw.css"
+import React, {useState, useEffect} from "react";
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
 
 const Editor = () => {
+  const [darkMode, setDarkMode] = useState()
+
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw />
     </div>
   );
-}
+};
 
-export default Editor
+export default Editor;
